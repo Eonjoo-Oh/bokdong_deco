2095d35e861df60e03debee1c57c7368bda9c4ba
Eonjoo-Oh <eoh@student.42seoul.kr>
2024-09-03 17:20:50 +0900

    Add: nav 복동 클릭시 복동 객체 생성 및 복동 배열에 추가

--- app.js
+++ app.js
@@ -5,15 +5,47 @@ const bokdongCtx = bokdongCanvas.getContext("2d");
 
 const background = document.getElementById("background");
 const bokdong = document.getElementById("bokdong");
+const bokdongSizeSlider = document.getElementById("bokdongSizeSllider");
 
 backgroundCanvas.width = 700;
 backgroundCanvas.height = 700;
 bokdongCanvas.width = 700;
 bokdongCanvas.height = 700;
 
+//----------------------복동 객체 생성 -----------------------
 
-function onBgClick(event) {
-	console.log("onbgclick!");
+class Bokdong {
+	constructor(imgSrc, x, y, width, height, id) {
+		this.id = id;
+		this.imgSrc = imgSrc;
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.image = new Image();
+		this.image.src = imgSrc;
+	}
+
+	drawBokdong(bokdongCtx) {
+		bokdongCtx.drawImage(this.image, this.x, this.y, this.width, this.height);
+	}
+
+	resize(newWidth, newHeight) {
+		this.width = newWidth;
+		this.height = newHeight;
+	}
+}
+
+let bokdongId = 0;
+function generateBokdongId() {
+	return ++bokdongId;
+}
+
+const bokdongArr = [];
+//---------------------------------------
+
+function onBackgroundClick(event) {
+	console.log("onbackgroundclick!");
 	if (event.target.tagName === 'IMG') {
 		const imgSrc = event.target.getAttribute('src');
 		const img = new Image();
@@ -26,18 +58,17 @@ function onBgClick(event) {
 	}
 }
 
-function onBokdongClick(event) {
-	console.log("onbockdongclick!");
+function onBokdongNavClick(event) {
+	console.log("onbockdongNavclick!");
 	if (event.target.tagName === 'IMG') {
 		const imgSrc = event.target.getAttribute('src');
-		const img = new Image();
-		img.src = imgSrc;
+		const newBokdong = new Bokdong(imgSrc, 0, 0, 100, 100, generateBokdongId());
+		bokdongArr.push(newBokdong);
+		bokdongCtx.drawImage(newBokdong.image, 0, 0, bokdongCanvas.width, bokdongCanvas.height);
 
-		img.onload = function() {
-			bokdongCtx.drawImage(img, 0, 0, bokdongCanvas.width, bokdongCanvas.height);
-		};
+		console.log(bokdongArr);
 	}
 }
 
-background.addEventListener("click", onBgClick);
-bokdong.addEventListener("click", onBokdongClick);
+background.addEventListener("click", onBackgroundClick);
+bokdong.addEventListener("click", onBokdongNavClick);
